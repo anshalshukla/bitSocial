19a9529c415be337707e0e86f0ee378f3940df8c
Anshal Shukla <shukla.anshal85@gmail.com>
2020-01-29 16:17:17 +0530

    .

--- recruitment_task/settings.py
+++ recruitment_task/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = keyconfig.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1", "localhost", "www.localhost.com"]
 
 
 # Application definition
