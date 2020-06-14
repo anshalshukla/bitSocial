bc031f310e6833619af7b2f2a4947b180d591b53
anshalshukla <shukla.anshal85@gmail.com>
2020-06-14 15:57:06 +0530

    shifted back to dbsqlite

--- recruitment_task/settings.py
+++ recruitment_task/settings.py
@@ -82,15 +82,21 @@ WSGI_APPLICATION = "recruitment_task.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.mysql",
+#         "NAME": keyconfig.DB_NAME,
+#         "USER": keyconfig.DB_USER,
+#         "PASSWORD": keyconfig.DB_PASSWORD,
+#     }
+# }
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.mysql",
-        "NAME": keyconfig.DB_NAME,
-        "USER": keyconfig.DB_USER,
-        "PASSWORD": keyconfig.DB_PASSWORD,
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
